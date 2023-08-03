9c27ad3d75efa74ec6f61c0ac02c7b67fb6474a5
Mohammed-M-Waloud <mohammedwaloud@gmail.com>
2023-08-03 07:49:04 -0700

    add job html templates

--- job/urls.py
+++ job/urls.py
@@ -4,6 +4,6 @@ from django.urls import path , include
 from . import views  # " . " tis mean the views in the same path 
 
 urlpatterns = [
-    path('', views.job_list), # type: ignore
-    path('<int:id>', views.job_details), # type: ignore
+    path('', views.job_list),
+    path('<int:id>', views.job_details),
 ]
